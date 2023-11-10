d1285bdf7b74cedbbc5e74789b49475350fe9c02
Jenya <113167825+Genya10@users.noreply.github.com>
2023-11-10 19:10:19 +0200

    add 12 tasks basic level

---
+++ freecodecampBasic/js1.js
@@ -0,0 +1,61 @@
+//1 task
+//i want to become a frontend developer
+/*i like freecodecamp and i study her*/
+
+//2
+var myName;
+
+//3
+var a;
+a=7;
+
+var a;
+a = 7;
+var b;
+b=a;
+
+//4
+myVar =0;
+var a =9;
+
+//5
+var myFirstName = "Jenya";
+var myLastName = "Kuziashov";
+
+//6
+var a=5;
+var b=10;
+var c='I am a';
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//7
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase= "A String";
+titleCaseOver= 9000;
+
+//8
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//9
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+//10
+const sum = 10 + 10;
+
+//11
+const difference = 45 -33;
+
+//12
+const product = 8 * 10;
