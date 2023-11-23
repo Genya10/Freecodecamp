214ef14f960a13aef5d084231ed3d783fb95b8c0
Jenya <113167825+Genya10@users.noreply.github.com>
2023-11-23 20:10:41 +0200

    add few tasks basic level

--- freecodecampBasic/js4.js
+++ freecodecampBasic/js4.js
@@ -102,3 +102,84 @@ function testElse(val) {
   console.log(caseInSwitch(1));
   console.log(caseInSwitch(3));
   console.log(caseInSwitch(5));
+
+  //7
+  function switchOfStuff(val) {
+    let answer = "";
+    switch(val){
+      case 'a':
+        answer = 'apple';
+        break;
+      case 'b':
+        answer= 'bird';
+      break;
+        case 'c':
+        answer ='cat';
+      break;
+        default:
+        answer = 'stuff';
+      
+    }
+      return answer;
+  }
+  
+  switchOfStuff(1);
+
+  //8
+  function sequentialSizes(val) {
+    let answer = "";
+
+    switch (val) {
+      case 1:
+      case 2:
+      case 3:
+       console.log(answer = "Low") ;
+        break;
+      case 4:
+      case 5:
+      case 6:
+        answer = "Mid";
+        break;
+      case 7:
+      case 8:
+      case 9:
+        answer = "High";
+    }
+
+    return answer;
+  }
+
+  sequentialSizes(1);
+
+  //9
+  function chainToSwitch(val) {
+    let answer = "";
+
+    switch (val) {
+      case "bob":
+        answer = "Marley";
+        break;
+      case 42:
+        answer = "The Answer";
+        break;
+      case 1:
+        answer = "There is no #1";
+        break;
+      case 99:
+        answer = "Missed me by this much!";
+        break;
+      case 7:
+        answer = "Ate Nine";
+        break;
+    }
+    return answer;
+  }
+
+  chainToSwitch(7);
+
+  //10
+  function isLess(a, b) {
+  return a < b;
+  }
+  
+  isLess(10, 15);
