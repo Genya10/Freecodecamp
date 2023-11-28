7e8d46d99ac8ffa2d0207747b023ece85d6fef84
Jenya <113167825+Genya10@users.noreply.github.com>
2023-11-28 20:16:08 +0200

    add few tasks

--- freecodecampBasic/js5.js
+++ freecodecampBasic/js5.js
@@ -51,3 +51,66 @@ const myDog2 = {
       return "Not Found";
     }
   }
+
+  //6
+  const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },{
+    "artist":"Jo Cocer",
+    "title":"Boys",
+    "release_year": 1987,
+    "formats":[
+      "CD",
+      "16T"
+    ]
+  }
+];
+
+//7
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+const gloveBoxContents = myStorage.car.inside['glove box'];
+console.log(gloveBoxContents);
+
+//8
+const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+const secondTree = myPlants[1].list[1];
+
+//9
