4dcbb40e845d6f54e8cf263f007adcda97c31e49
Jenya <113167825+Genya10@users.noreply.github.com>
2023-12-01 18:27:49 +0200

    add few tasks basic level

--- freecodecampBasic/js6.js
+++ freecodecampBasic/js6.js
@@ -35,6 +35,36 @@ console.log(sum([2,4,6],3));
 console.log(sum([6,8],0));
 console.log(sum([0,2],2));
 
+const array =[12,45,65];
+let count = 0;
+for(let i =0;i< array.length;i++){
+  count += array[i];
+}
+console.log(count)
 
+//4
+function checkEqual(a, b) {
+  return a === b ? "Equal":"Not Equal"
+}
+checkEqual(1, 2);
 
-  
+//5
+function checkSign(num) {
+  return num > 0 ? 'positive' 
+   : num < 0 ? 'negative'
+   : 'zero'
+  }
+  checkSign(10);
+
+  //6
+  function convertToInteger(str) {
+    return parseInt(str)
+ }
+ convertToInteger("56")
+
+ //7
+ function convertToInteger(str) {
+  return parseInt(str,2)
+ }
+ 
+ console.log(convertToInteger("10011"))
